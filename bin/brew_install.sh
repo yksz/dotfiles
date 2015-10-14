e3d1c6f677ec2021d8b7f6b109b4350143dee96d
yksz <user4git@gmail.com>
2015-10-14 22:34:43 +0900

    Add bin/

---
+++ bin/brew_install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# tools
+brew install vim
+brew install zsh
+brew install tree
+brew install tmux
+brew install zlib
+brew install opencv
+brew install openssl
+
+# lang
+brew install groovy
+brew install gauche
+brew install guile
+brew install node
+brew install ocaml
+brew install scala
+
+# lang's tools
+brew install cmake   # c
+brew install gradle  # java
+brew install sbt     # scala
+brew install ctags   # c, go
+brew install glib    # c
+brew install jenkins # all
+
+# db
+brew install sqlite
+brew install postgresql
