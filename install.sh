04aca3081cb75329c27c429e8ad2bfb1296eba25
yksz <user4git@gmail.com>
2015-10-15 10:36:03 +0900

    Add .wgetrc

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 BASE_DIR=$(cd $(dirname $0); pwd)
 ln -sf ${BASE_DIR}/.bash_profile ~/.bash_profile
+ln -sf ${BASE_DIR}/.ctags        ~/.ctags
+ln -sf ${BASE_DIR}/.gitconfig    ~/.gitconfig
 ln -sf ${BASE_DIR}/.vimrc        ~/.vimrc
 ln -sf ${BASE_DIR}/.vimrc.plugin ~/.vimrc.plugin
+ln -sf ${BASE_DIR}/.wgetrc       ~/.wgetrc
 ln -sf ${BASE_DIR}/.zshrc        ~/.zshrc
-ln -sf ${BASE_DIR}/.gitconfig    ~/.gitconfig
-ln -sf ${BASE_DIR}/.ctags        ~/.ctags
