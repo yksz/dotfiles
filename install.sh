918c3bbfd6e7395024bf3945061eca3c9b4c25d5
yksz <user4git@gmail.com>
2016-02-05 16:23:40 +0900

    Add .tmux.conf

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 script_dir=$(cd $(dirname $0); pwd)
 ln -sf ${script_dir}/.bash_profile       ~/.bash_profile
-ln -sf ${script_dir}/.bash_hrc           ~/.bash_hrc
+ln -sf ${script_dir}/.bashrc             ~/.bashrc
 ln -sf ${script_dir}/.ctags              ~/.ctags
 ln -sf ${script_dir}/.emacs              ~/.emacs
 ln -sf ${script_dir}/.gitconfig          ~/.gitconfig
 ln -sf ${script_dir}/.proxy.sh           ~/.proxy.sh
+ln -sf ${script_dir}/.tmux.conf          ~/.tmux.conf
 ln -sf ${script_dir}/.vimrc              ~/.vimrc
 ln -sf ${script_dir}/.vimrc.plugin       ~/.vimrc.plugin
 #ln -sf ${script_dir}/.vimrc.plugin.light ~/.vimrc.plugin.light
