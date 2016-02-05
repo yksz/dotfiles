8d8cb1bedfe926f2e85d93c779b6bf278b3adf80
yksz <user4git@gmail.com>
2016-02-05 12:31:58 +0900

    Add .zprofile

---
+++ .zprofile
@@ -0,0 +1,2 @@
+### 外部ファイルの読み込み
+[ -f ~/.bash_profile ] && source ~/.bash_profile
--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 script_dir=$(cd $(dirname $0); pwd)
 ln -sf ${script_dir}/.bash_profile       ~/.bash_profile
+ln -sf ${script_dir}/.bash_hrc           ~/.bash_hrc
 ln -sf ${script_dir}/.ctags              ~/.ctags
 ln -sf ${script_dir}/.emacs              ~/.emacs
 ln -sf ${script_dir}/.gitconfig          ~/.gitconfig
 ln -sf ${script_dir}/.vimrc              ~/.vimrc
 ln -sf ${script_dir}/.vimrc.plugin       ~/.vimrc.plugin
 #ln -sf ${script_dir}/.vimrc.plugin.light ~/.vimrc.plugin.light
+ln -sf ${script_dir}/.zprofile           ~/.zprofile
 ln -sf ${script_dir}/.zshrc              ~/.zshrc
