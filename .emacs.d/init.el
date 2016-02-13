2511aab81bcde9fc059c7db35915e93a993af6b0
yksz <user4git@gmail.com>
2016-02-13 10:40:21 +0900

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,10 @@
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
+;; clipboard
+(setq x-select-enable-clipboard t)
+
+;; el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
@@ -11,8 +15,14 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
 
+;; el-get-bundle
 (el-get-bundle evil)
 (el-get-bundle auto-complete)
 
 (require 'evil)
 (evil-mode 1)
+
+(require 'auto-complete-config)
+(ac-config-default)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)
