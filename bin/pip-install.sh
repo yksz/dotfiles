bce7eb0d982261a3e2c61494149a354425bb87cf
yksz <user4git@gmail.com>
2016-03-01 23:46:54 +0900

    Refactor pip-install.sh

--- bin/pip-install.sh
+++ bin/pip-install.sh
@@ -4,6 +4,6 @@ DOWNLOAD_DIR="/tmp"
 
 cd $DOWNLOAD_DIR
 wget https://bootstrap.pypa.io/get-pip.py
-proxy=`echo $http_proxy | sed -e "s/http:\/\///"`
+proxy=`echo $http_proxy | sed -e 's/http:\/\///'` # remove 'http://' from $http_proxy
 python get-pip.py --proxy=$proxy
 rm get-pip.py
