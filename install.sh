7ca7567eeb7d33dbf6394322a50c3b7e1c90377b
yksz <yksz.dev@gmail.com>
2016-08-08 15:25:32 +0900

    install.sh: Refactor

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 
 mkdir -p ~/.emacs.d
 
-script_dir=$(cd $(dirname $0); pwd)
-ln -sf ${script_dir}/.bash_aliases       ~/.bash_aliases
-ln -sf ${script_dir}/.bashrc             ~/.bashrc
-ln -sf ${script_dir}/.ctags              ~/.ctags
-ln -sf ${script_dir}/.emacs.d/init.el    ~/.emacs.d/init.el
-ln -sf ${script_dir}/.gitconfig          ~/.gitconfig
-ln -sf ${script_dir}/.profile            ~/.profile
-#ln -sf ${script_dir}/.proxy.sh           ~/.proxy.sh
-ln -sf ${script_dir}/.tmux.conf          ~/.tmux.conf
-ln -sf ${script_dir}/.vimrc              ~/.vimrc
-#ln -sf ${script_dir}/.vimrc.light        ~/.vimrc
-#ln -sf ${script_dir}/.vimrc.plugin       ~/.vimrc.plugin
-ln -sf ${script_dir}/.vimrc.plugin.light ~/.vimrc.plugin
-ln -sf ${script_dir}/.zshenv             ~/.zshenv
-ln -sf ${script_dir}/.zshrc              ~/.zshrc
+dotfiles_dir=$(cd $(dirname $0); pwd)
+ln -sf ${dotfiles_dir}/.bash_aliases       ~/.bash_aliases
+ln -sf ${dotfiles_dir}/.bashrc             ~/.bashrc
+ln -sf ${dotfiles_dir}/.ctags              ~/.ctags
+ln -sf ${dotfiles_dir}/.emacs.d/init.el    ~/.emacs.d/init.el
+ln -sf ${dotfiles_dir}/.gitconfig          ~/.gitconfig
+ln -sf ${dotfiles_dir}/.profile            ~/.profile
+#ln -sf ${dotfiles_dir}/.proxy.sh           ~/.proxy.sh
+ln -sf ${dotfiles_dir}/.tmux.conf          ~/.tmux.conf
+ln -sf ${dotfiles_dir}/.vimrc              ~/.vimrc
+#ln -sf ${dotfiles_dir}/.vimrc.light        ~/.vimrc
+#ln -sf ${dotfiles_dir}/.vimrc.plugin       ~/.vimrc.plugin
+ln -sf ${dotfiles_dir}/.vimrc.plugin.light ~/.vimrc.plugin
+ln -sf ${dotfiles_dir}/.zshenv             ~/.zshenv
+ln -sf ${dotfiles_dir}/.zshrc              ~/.zshrc
