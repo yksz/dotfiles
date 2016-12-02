fbf21e216b7d7d3adcbf108ba7e30d2547fd1a1c
yksz <yksz.dev@gmail.com>
2016-12-03 00:35:06 +0900

    emacs: Divide init.el into files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,28 +1,6 @@
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
-
-;; clipboard
+;; Enable clipboard
 (setq x-select-enable-clipboard t)
 
-;; el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-
-;; el-get-bundle
-(el-get-bundle evil)
-(el-get-bundle auto-complete)
-
-(require 'evil)
-(evil-mode 1)
-
-(require 'auto-complete-config)
-(ac-config-default)
-(ac-set-trigger-key "TAB")
-(setq ac-use-menu-map t)
+;; Load external files
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(load "packages")
--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-mkdir -p ~/.emacs.d
+mkdir -p ~/.emacs.d/lisp
 
 dotfiles_dir=$(cd $(dirname $0); pwd)
-ln -sf ${dotfiles_dir}/.aliases            ~/.aliases
-#ln -sf ${dotfiles_dir}/.bashrc             ~/.bashrc
-ln -sf ${dotfiles_dir}/.ctags              ~/.ctags
-ln -sf ${dotfiles_dir}/.emacs.d/init.el    ~/.emacs.d/init.el
-ln -sf ${dotfiles_dir}/.gitconfig          ~/.gitconfig
-ln -sf ${dotfiles_dir}/.profile            ~/.profile
-#ln -sf ${dotfiles_dir}/.proxy.sh           ~/.proxy.sh
-ln -sf ${dotfiles_dir}/.tmux.conf          ~/.tmux.conf
-ln -sf ${dotfiles_dir}/.vimrc              ~/.vimrc
-#ln -sf ${dotfiles_dir}/.vimrc.light        ~/.vimrc
-#ln -sf ${dotfiles_dir}/.vimrc.plugin       ~/.vimrc.plugin
-ln -sf ${dotfiles_dir}/.vimrc.plugin.light ~/.vimrc.plugin
-ln -sf ${dotfiles_dir}/.zshenv             ~/.zshenv
-ln -sf ${dotfiles_dir}/.zshrc              ~/.zshrc
+ln -sf ${dotfiles_dir}/.aliases                  ~/.aliases
+#ln -sf ${dotfiles_dir}/.bashrc                   ~/.bashrc
+ln -sf ${dotfiles_dir}/.ctags                    ~/.ctags
+ln -sf ${dotfiles_dir}/.emacs.d/init.el          ~/.emacs.d/init.el
+ln -sf ${dotfiles_dir}/.emacs.d/lisp/packages.el ~/.emacs.d/lisp/packages.el
+ln -sf ${dotfiles_dir}/.gitconfig                ~/.gitconfig
+ln -sf ${dotfiles_dir}/.profile                  ~/.profile
+#ln -sf ${dotfiles_dir}/.proxy.sh                 ~/.proxy.sh
+ln -sf ${dotfiles_dir}/.tmux.conf                ~/.tmux.conf
+ln -sf ${dotfiles_dir}/.vimrc                    ~/.vimrc
+#ln -sf ${dotfiles_dir}/.vimrc.light              ~/.vimrc
+#ln -sf ${dotfiles_dir}/.vimrc.plugin             ~/.vimrc.plugin
+ln -sf ${dotfiles_dir}/.vimrc.plugin.light       ~/.vimrc.plugin
+ln -sf ${dotfiles_dir}/.zshenv                   ~/.zshenv
+ln -sf ${dotfiles_dir}/.zshrc                    ~/.zshrc
