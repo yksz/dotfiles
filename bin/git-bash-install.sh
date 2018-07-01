bc9345cdc46becec4d4661fb942dd01ed6fde75e
yksz <yksz.dev@gmail.com>
2018-07-01 15:10:25 +0900

    bash: Change bash options

---
+++ bin/git-bash-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -O ~/.git-prompt.sh https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
+wget -O ~/.git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
