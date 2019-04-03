06bd4d220c2e904dc14370f3a94e4e986976c0c9
yksz <yksz.dev@gmail.com>
2019-04-03 12:40:34 +0900

    vim: Use Vim-plug instead of NeoBundle

--- bin/neobundle-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-VERSION=ver.3.2
-
-mkdir -p ~/.vim/bundle
-git clone --depth=1 -b $VERSION https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
---
+++ bin/vim-plug_install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p ~/.vim/autoload
+wget -O ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
