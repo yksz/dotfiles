9a77ca31bd395fb945e3c304b045586ee16ca392
yksz <yksz.dev@gmail.com>
2019-07-01 14:59:54 +0900

    bin: Add the package install script for Chocolatey

---
+++ bin/chocolatey-package-install.bat
@@ -0,0 +1,11 @@
+@rem Command line tools
+choco install -y wget
+choco install -y 7zip.commandline
+@rem C#
+choco install -y nuget.commandline
+
+@rem Utility tools
+choco install -y vivaldi
+
+@rem Development tools
+choco install -y vscode
