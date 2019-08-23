c7bb5f09eddb54f5cf4b21a8b1f14a331b4b3dcb
yksz <yksz.dev@gmail.com>
2019-08-23 16:35:14 +0900

    bin: Add the install script for Ubuntu packages

---
+++ bin/ubuntu-package-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo -E apt-get install -y ssh
+sudo -E apt-get install -y vim
+sudo -E apt-get install -y git
+sudo -E apt-get install -y build-essential
+sudo -E apt-get install -y cmake
+sudo -E apt-get install -y sysstat
