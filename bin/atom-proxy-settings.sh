61b773e4107da2c2d9c0fc191ca69f525ba0b012
yksz <yksz.dev@gmail.com>
2019-08-23 12:54:00 +0900

    bin: Delete the unnecessary scripts

--- bin/atom-proxy-settings.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-apm config set https-proxy $http_proxy
--- bin/capslock2ctrl_ubuntu14.04.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-dconf reset /org/gnome/settings-daemon/plugins/keyboard/active
-dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
